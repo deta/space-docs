4b3738584b06a5c2b8e7b45bd23ddb5051c21bde
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-05-17 00:32:59 +0200

    Adds IconButton component

---
+++ src/components/core/Button/IconButton.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  // TODO(@maxu): rework this thing
+  export let type: "button" | "link" = "button",
+    style: "ghost" | "link" = "ghost",
+    target: "_blank" | "_self" = "_self",
+    href: string = "",
+    disabled = false;
+</script>
+
+<button class:disabled on:click {disabled}>
+  <span><slot /></span>
+</button>
+
+<style>
+  button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 7px 7px;
+    border-radius: 10px;
+    background: transparent;
+    border: 1px solid transparent;
+    cursor: pointer;
+    transform: scale(1);
+
+    transition: all 0.05s ease-in-out, background 0.15s ease-in-out;
+  }
+  button:not(.disabled):hover {
+    background: hsl(var(--color-gray-90));
+  }
+  button:not(.disabled):active > span {
+    transform: scale(0.85);
+  }
+
+  button > span {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
