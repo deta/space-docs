7db0ac031ba02deba5a2163cf2565116f588d16f
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-05-17 00:30:55 +0200

    Adds serveral icons

---
+++ src/components/core/Icon/IconExternalLink.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Icon from "./Icon.svelte";
+</script>
+
+<Icon {...$$restProps}>
+<path d="M11 7H6C4.89543 7 4 7.89543 4 9V18C4 19.1046 4.89543 20 6 20H15C16.1046 20 17 19.1046 17 18V13" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M10 14L20 4" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M15 4H20V9" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+</Icon>
