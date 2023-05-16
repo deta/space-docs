2f9a1cc8f727d1bbc397cd470a1059d5bd0fefe9
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-05-17 00:28:07 +0200

    Adds docs content collection

---
+++ src/content/config.ts
@@ -0,0 +1,19 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// 2. Define your collection(s)
+const docsCollection = defineCollection({
+    schema: z.object({
+        title: z.string(),
+        excerpt: z.string().optional()
+    })
+});
+/*const changelogCollection = defineCollection({});
+const blogCollection = defineCollection({});
+*/
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+    docs: docsCollection,
+    //changelog: changelogCollection,
+    //blog: blogCollection
+};
