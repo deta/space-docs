ecdc661a6e2ec2292645784d7b039363b74eb922
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-05-17 00:28:50 +0200

    format document

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,17 +10,18 @@ export default defineConfig({
   vite: {
     build: {
       rollupOptions: {
-        external: process.env.PUBLIC_TELETYPE_INSTALLED === 'true' ? [] : ['@deta/teletype/src/index'],
+        external:
+          process.env.PUBLIC_TELETYPE_INSTALLED === "true" ? [] : ["@deta/teletype/src/index"],
         output: {
           assetFileNames: (assetInfo) => {
-            let extType = assetInfo.name.split('.').at(1);
-            if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) extType = 'img';
+            let extType = assetInfo.name.split(".").at(1);
+            if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) extType = "img";
             return `docs_assets/${extType}/[name]-[hash][extname]`;
           },
-          chunkFileNames: 'docs_chunks/[name]-[hash].js',
-          entryFileNames: 'docs_assets/js/[name]-[hash].js',
-        },
-      },
+          chunkFileNames: "docs_chunks/[name]-[hash].js",
+          entryFileNames: "docs_assets/js/[name]-[hash].js"
+        }
+      }
     }
   }
 });
