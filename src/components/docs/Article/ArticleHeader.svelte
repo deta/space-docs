cdb741ae31358fb22873ecb89d12d5cdd80a31a4
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-05-17 03:31:47 +0200

    Adds Article components & style

---
+++ src/components/docs/Article/ArticleHeader.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    export let title: string, excerpt: string | undefined;
+</script>
+
+<section>
+    <h1>{title}</h1>
+    {#if excerpt}
+    <p class="lead">{excerpt}</p>
+    <hr/>
+    {/if}
+</section>
+
+<style>
+    section {
+        margin-bottom: var(--spacing-12);
+    }
+
+    hr {
+        margin-block: var(--spacing-6);
+        color: hsl(var(--color-gray-95));
+    }
+</style>
