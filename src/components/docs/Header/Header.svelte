8bb337ac76bc57dc98960c4ce5d00afccbf1aed9
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-05-17 03:30:31 +0200

    Adds docs header & theme toggle components

---
+++ src/components/docs/Header/Header.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+  import IconButton from "@/components/core/Button/IconButton.svelte";
+  import ThemeToggle from "./ThemeToggle.svelte";
+  import IconMenu2 from "@/components/core/Icon/IconMenu2.svelte";
+  import { toggleSideNav } from "../SideNav/SideNav.svelte";
+
+  function onToggleNav() {
+    toggleSideNav();
+  }
+</script>
+
+<header>
+  <!-- TODO(@maxu): Skip to content link-->
+  <!-- LEFT SIDE -->
+  <div>
+    <ul>
+      <li>
+        <IconButton on:click={onToggleNav}
+          ><IconMenu2 size={24} strokeWidth={2} /></IconButton>
+      </li>
+      <li class="only-tablet">breadcrumbs</li>
+    </ul>
+  </div>
+
+  <!-- RIGHT SIDE-->
+  <div class="only-tablet">
+    <ul>
+      <li>langPicker</li>
+      <li><ThemeToggle /></li>
+    </ul>
+  </div>
+</header>
+
+<style lang="scss">
+  //@use "@/styles/breakpoints";
+  header {
+    grid-column: 1 / -1;
+    grid-row: 1 / -1;
+    position: fixed;
+    z-index: 20;
+    bottom: 0; // TODO: Add bottom margin to container
+    left: 0;
+    right: 0;
+    @media screen and (min-width: 768px) {
+        grid-column: 1 / -1;
+        grid-row: 1;
+        position: unset;
+        top: 0;
+        bottom: unset;
+    }
+
+    display: flex;
+    justify-content: flex-end;
+    @media screen and (min-width: 768px) {
+        justify-content: space-between;
+    }
+    align-items: center;
+    padding-inline: var(--spacing-2);
+    padding-block: var(--spacing-2);
+
+    background: var(--theme-navbar-bg);
+
+    ul {
+      display: flex;
+      align-items: center;
+      gap: var(--spacing-8);
+    }
+
+  }
+  :global(body.sideNav-open header) {
+    grid-column: 2 / -1 !important;
+  }
+
+  .only-tablet {
+    display: none;
+    @media screen and (min-width: 768px) {
+      display: unset;
+    }
+  }
+</style>
---
+++ src/components/docs/Header/ThemeToggle.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import IconButton from "@/components/core/Button/IconButton.svelte";
+  import IconRocket from "@/components/core/Icon/IconRocket.svelte";
+  import IconSun from "@/components/core/Icon/IconSun.svelte";
+
+  // TODO: IMPL
+  let theme: "light" | "dark" = "light";
+
+  function onToggleTheme() {
+    theme === "light" ? (theme = "dark") : (theme = "light");
+    if (theme === 'dark') {
+      document.documentElement.classList.add('theme-dark')
+    } else {
+      document.documentElement.classList.remove('theme-dark')
+    }
+  }
+</script>
+
+<IconButton
+  on:click={onToggleTheme}>
+  {#if theme === "light"}
+    <IconSun />
+  {:else}
+    <IconRocket />
+  {/if}
+</IconButton>
