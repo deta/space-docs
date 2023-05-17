885a42d28931e0f0476d7752de52ef09ae318d8f
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-05-17 03:30:08 +0200

    Adds docs footer component

---
+++ src/components/docs/Footer/Footer.svelte
@@ -0,0 +1,27 @@
+<footer>
+  <div class="content">
+    <p class="title">DETA ~ 2335</p>
+    <p>
+      <a href="https://deta.space/legal">Legal</a> /
+      <a href="https://deta.space/privacy">Privacy</a>
+      / <a href="https://deta.space/terms">Terms</a>
+    </p>
+  </div>
+</footer>
+
+<style lang="scss">
+    footer {
+        grid-column: 1 / -1;
+        grid-row: 3;
+
+        padding: var(--spacing-4);
+
+        // TODO!: FIX
+        background: #191818;
+        color: white;
+    }
+
+    :global(body.sideNav-open footer) {
+        grid-column: 2 / -1 !important;
+    }
+</style>
