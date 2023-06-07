00d911803e75392adc36e20899bc1ff6593cbae7
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-06-07 10:07:42 +0200

    fix: theme toggle respect user setting & persist

--- src/components/docs/Header/ThemeToggle.svelte
+++ src/components/docs/Header/ThemeToggle.svelte
@@ -1,23 +1,47 @@
+<script lang="ts" context="module">
+  export type Theme = "light" | "dark";
+
+  export function getSystemTheme(): Theme {
+    return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+  }
+
+  const themeStore = storedRawWritable<Theme | "null">("theme", "null");
+
+  export function setTheme(theme: Theme) {
+    themeStore.set(theme);
+    if (theme === "dark") {
+      document.documentElement.classList.add("theme-dark");
+    } else {
+      document.documentElement.classList.remove("theme-dark");
+    }
+  }
+</script>
+
 <script lang="ts">
   import IconButton from "@/components/core/Button/IconButton.svelte";
   import IconMoon from "@/components/core/Icon/IconMoon.svelte";
   import IconSun from "@/components/core/Icon/IconSun.svelte";
+  import { storedRawWritable } from "@/utils/storedWritable";
+  import { onMount } from "svelte";
+  import { get } from "svelte/store";
 
   // TODO: IMPL
   let theme: "light" | "dark" = "light";
 
   function onToggleTheme() {
-    theme === "light" ? (theme = "dark") : (theme = "light");
-    if (theme === 'dark') {
-      document.documentElement.classList.add('theme-dark')
-    } else {
-      document.documentElement.classList.remove('theme-dark')
-    }
+    const newTheme = get(themeStore) === "light" ? "dark" : "light";
+    setTheme(newTheme);
   }
+
+  onMount(() => {
+    const currentTheme = get(themeStore);
+    if (currentTheme === "null") {
+      themeStore.set(getSystemTheme());
+    }
+  });
 </script>
 
-<IconButton
-  on:click={onToggleTheme}>
+<IconButton on:click={onToggleTheme}>
   {#if theme === "light"}
     <IconSun />
   {:else}
