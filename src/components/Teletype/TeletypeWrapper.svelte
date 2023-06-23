d8a3c54d4c7c1bb6295651f429415938f219c14a
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2023-06-23 08:48:31 +0200

    try injecting tty class and increase z-index

--- src/components/Teletype/TeletypeWrapper.svelte
+++ src/components/Teletype/TeletypeWrapper.svelte
@@ -8,15 +8,25 @@ import { onMount } from 'svelte';
 
 const useTeletype = import.meta.env.PUBLIC_TELETYPE_INSTALLED === 'true'
 
-let Teletype: any
+let Teletype: any;
+let ttyElement: HTMLElement;
 onMount(async () => {
     if (useTeletype) {
         const importModule = await import('./Teletype.svelte')
         Teletype = importModule.default
     }
+    if (ttyElement) {
+        ttyElement.classList.add("tty");
+    }
 })
 </script>
 
 {#if Teletype}
-    <svelte:component this={Teletype}/>
+    <svelte:component bind:this={ttyElement} this={Teletype}/>
 {/if}
+
+<style lang="scss">
+    :global(.tty) {
+        z-index: 200;
+    }
+</style>
