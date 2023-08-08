36fc73a431e77adf8e6fff5b76dbf5a1f24e0627
Maximilian Heidenreich <github@maximilian-heidenreich.de>
2023-08-08 15:02:09 +0200

    removed non working tty styling hjack

--- src/components/Teletype/TeletypeWrapper.svelte
+++ src/components/Teletype/TeletypeWrapper.svelte
@@ -9,24 +9,14 @@ import { onMount } from 'svelte';
 const useTeletype = import.meta.env.PUBLIC_TELETYPE_INSTALLED === 'true'
 
 let Teletype: any;
-let ttyElement: HTMLElement;
 onMount(async () => {
     if (useTeletype) {
         const importModule = await import('./Teletype.svelte')
         Teletype = importModule.default
     }
-    if (ttyElement) {
-        ttyElement.classList.add("tty");
-    }
 })
 </script>
 
 {#if Teletype}
-    <svelte:component bind:this={ttyElement} this={Teletype}/>
+    <svelte:component this={Teletype} />
 {/if}
-
-<style lang="scss">
-    :global(.tty) {
-        z-index: 200;
-    }
-</style>
